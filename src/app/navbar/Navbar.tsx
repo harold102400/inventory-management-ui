bf803c9b9fa69942b1427d652900a884d975f3f6
harold montes <haroldjosehd24@gmail.com>
2024-05-30 15:11:26 -0400

    implement navbar component and styling

--- src/app/navbar/Navbar.tsx
+++ src/app/navbar/Navbar.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import {Link, NavLink} from 'react-router-dom';
+import './Navbar.css'
 
 
 const Navbar = () => {
+  const [ menuOpen, setMenuOpen ] = useState<Boolean>(false);
+
   return (
-    <div>Navbar</div>
+ <nav>
+  <Link to="/" className="title">
+    Dashboard
+  </Link>
+  <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <ul className={menuOpen ? "open" : ""}>
+    <li>
+      <NavLink to="/contactus">Contactame</NavLink>
+    </li>
+  </ul>
+ </nav>
   )
 }
 
